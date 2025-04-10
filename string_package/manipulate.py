adbab308169b5fc42bad66d0584d1d52f6349ed5
Mustafa Eren Şahin <152656162+MustafaErenSahin@users.noreply.github.com>
2025-04-10 19:19:29 +0700

    Update manipulate.py

--- string_package/manipulate.py
+++ string_package/manipulate.py
@@ -1,1 +1,10 @@
+import string
 
+def reverse_string(s):
+    return s[::-1]
+
+def capitalize_words(s):
+    return s.upper()
+
+def remove_punctuation(s):
+    return s.translate(str.maketrans('', '', string.punctuation))
