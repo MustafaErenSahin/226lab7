99d5b5de5bebea81edd1eea5b7389fd6f5ec9152
Mustafa Eren Şahin <152656162+MustafaErenSahin@users.noreply.github.com>
2025-04-10 19:22:17 +0700

    Update string_analyzer.py

--- string_analyzer.py
+++ string_analyzer.py
@@ -1,1 +1,10 @@
+from string_package import count_characters, count_words, average_word_length, reverse_string, capitalize_words, remove_punctuation
 
+a = input("Enter a sentence: ")
+
+print("Reverse version: ", reverse_string(a))
+print("Capital version: ", capitalize_words(a))
+a = remove_punctuation(a)
+print("Total characters: ", count_characters(a))
+print("Total words: ", count_words(a))
+print("Average word length: ", average_word_length(a))
