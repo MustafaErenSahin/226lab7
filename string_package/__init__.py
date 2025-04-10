4fdc5b08d79b8e9aa21df017ea1bbbc25fa7a22f
Mustafa Eren Şahin <152656162+MustafaErenSahin@users.noreply.github.com>
2025-04-10 19:18:40 +0700

    Update __init__.py

--- string_package/__init__.py
+++ string_package/__init__.py
@@ -1,1 +1,9 @@
+from .stats import count_characters
+from .stats import count_words
+from .stats import average_word_length
+
+from .manipulate import reverse_string
+from .manipulate import capitalize_words
+from .manipulate import remove_punctuation
+
 
