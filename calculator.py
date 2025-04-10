7748eac91c6b3bb88399bc213928b985f5c01e28
Mustafa Eren Şahin <152656162+MustafaErenSahin@users.noreply.github.com>
2025-04-10 19:16:30 +0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,20 @@
+import math_utils
 
+def main():
+    a = int(input("Enter a number: "))
+    b = int(input("Enter another number: "))
+    op = input("Choose a operator: ")
+
+    operation = {'+': math_utils.add, '-': math_utils.subtract, '*': math_utils.multiply, '/': math_utils.divide, '^': math_utils.power, '%': math_utils.mod}
+
+    if op in operation:
+        try:
+            f = operation[op](a, b)
+            print(f)
+        except Exception as e:
+            print(e)
+    else:
+        print("Try again")
+
+if __name__ == "__main__":
+    main()
